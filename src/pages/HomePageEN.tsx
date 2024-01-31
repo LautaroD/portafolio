dc6309ed0d57e24980d69f7536f297061558d727
LautaroD <lautarodarretche@hotmail.es>
2024-01-31 10:46:05 -0300

    update

--- src/pages/HomePageEN.tsx
+++ src/pages/HomePageEN.tsx
@@ -1,38 +1,38 @@
-import React from 'react';
-import { VStack, Heading, Container, ColorModeContext, useColorMode } from '@chakra-ui/react';
-import { Footer, Header, Navbar, SkillSlider } from '../components';
-import { useAppSelector } from '../redux/hooks/redux-hooks';
-import ContactForm from '../components/ContactForm';
-import { DotPulse } from '@uiball/loaders';
-import Banner from '../components/Banner';
+import React from "react";
+import { VStack, Heading, useColorMode } from "@chakra-ui/react";
+import { Footer, Header, Navbar, SkillSlider } from "../components";
+import { useAppSelector } from "../redux/hooks/redux-hooks";
+import ContactForm from "../components/ContactForm";
+import { DotPulse } from "@uiball/loaders";
+import Banner from "../components/Banner";
 
-const Projects = React.lazy(() => import('../components/Projects'));
+const Projects = React.lazy(() => import("../components/Projects"));
 
 const HomePageEN = (): JSX.Element => {
-  const { colorMode } = useColorMode();
-  const { projects } = useAppSelector((state) => state.projects);
+    const { colorMode } = useColorMode();
+    const { projects } = useAppSelector((state) => state.projects);
 
-  return (
-    <VStack minH={'100vh'}>
-      <Navbar />
-      <Header />
-      <Heading id="my-skills" pt={28} pb={6} color={colorMode === 'dark' ? "brand.clear" : "brand.primary800"}>
-        My Skills
-      </Heading>
-      <SkillSlider />
-      <Banner />
+    return (
+        <VStack minH={"100vh"}>
+            <Navbar />
+            <Header />
+            <Heading id='my-skills' pt={28} pb={6} color={colorMode === "dark" ? "brand.clear" : "brand.primary800"}>
+                My Skills
+            </Heading>
+            <SkillSlider />
+            <Banner />
 
-      <Heading id="projects" pt={24} color={colorMode === 'dark' ? "brand.clear" : "brand.primary800"}>My Projects</Heading>
-      <React.Suspense
-        fallback={<DotPulse size={40} speed={1.3} color="#615AFF" />}
-      >
-        <Projects projects={projects !== null ? projects : []} />
-      </React.Suspense>
+            <Heading id='projects' pt={24} color={colorMode === "dark" ? "brand.clear" : "brand.primary800"}>
+                My Projects
+            </Heading>
+            <React.Suspense fallback={<DotPulse size={40} speed={1.3} color='#615AFF' />}>
+                <Projects projects={projects !== null ? projects : []} />
+            </React.Suspense>
 
-      <ContactForm />
-      <Footer />
-    </VStack>
-  );
+            <ContactForm />
+            <Footer />
+        </VStack>
+    );
 };
 
 export default HomePageEN;
